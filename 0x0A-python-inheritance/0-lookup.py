35de823a04d79f1f1a5f63606edd74cad696131b
ken-001-mk <mwaskenne@gmail.com>
2023-02-08 10:15:21 -0800

    inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""function that return list of available attributes and methods of object"""
+
+def lookup(obj):
+	"""function that looks for methods and attributes of objects"""
+	return dir(obj)
