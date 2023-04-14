2373f4bef496384484fc83372352da1bac1c2368
ken-001-mk <mwaskenne@gmail.com>
2023-04-14 11:31:46 +0000

    done tasks js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
 
-const arg1 = process.argv[3];
-if (arg1 === 0) {
-  console.log('Python is fun');
-} else if (arg1 === 2) {
-  console.log('HBTN is undefined');
-} else {
-  console.log('undefined is undefined');
-}
+console.log('Python is fun');
+console.log('HBTN is undefined');
+console.log('undefined is undefined');
